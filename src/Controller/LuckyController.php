604d74d9da7d6d2d131d9fe30beba210fbd6f66a
Melvin Petersson <melle0327@icloud.com>
2024-04-15 22:06:00 +0200

    kmom02

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -17,31 +17,4 @@ class LuckyController
             '<html><body>Lucky number: '.$number.'</body></html>'
         );
     }
-
-    // #[Route("/api/lucky/number")]
-    // public function jsonNumber(): Response
-    // {
-    //     $number = random_int(0, 100);
-
-    //     $data = [
-    //         'lucky-number' => $number,
-    //         'lucky-message' => 'Hi there!',
-    //     ];
-
-    //     // return new JsonResponse($data);
-
-    //     $response = new JsonResponse($data);
-    //     $response->setEncodingOptions(
-    //         $response->getEncodingOptions() | JSON_PRETTY_PRINT
-    //     );
-    //     return $response;
-    // }
-
-    // #[Route("/lucky/hi")]
-    // public function hi(): Response
-    // {
-    //     return new Response(
-    //         '<html><body>Hi to you!</body></html>'
-    //     );
-    // }
 }
